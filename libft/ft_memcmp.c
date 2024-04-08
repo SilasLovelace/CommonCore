b6eb62f97aa973981ebefc43e8812e9a6e102df9
Silas Oppermann <sopperma@c2r1p3.42vienna.com>
2024-04-08 10:45:33 +0200

    memcmp

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -25,7 +25,7 @@ int	ft_memcmp(const void *ptr1, const void *ptr2, size_t num)
 	while (s1[c] == s2[c] && c < num)
 		c++;
 	if (c == num)
-		(s1[c-1] - s2[c-1]);
+		return (s1[c-1] - s2[c-1]);
 	return (s1[c] - s2[c]);
 }
 /*
