129c83316b89b863e943729224045b0bb566688c
Silas Oppermann <silas.oppermann@gmail.com>
2025-03-20 15:58:31 +0100

    fixed makefiles

--- CPP00/ex00/megaphone.cpp
+++ CPP00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: sopperma <sopperma@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/02 14:09:29 by sopperma          #+#    #+#             */
-/*   Updated: 2025/01/02 14:26:02 by sopperma         ###   ########.fr       */
+/*   Updated: 2025/03/20 15:57:31 by sopperma         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,12 @@
 int main (int argc, char **argv) {
     if (argc > 1)
     {
-        int i{1};
+        int i = 1;
         while (i < argc) {  
-            int j{0};
+            int j = 0;
             while (argv[i][j]) {std::cout << (char)toupper(argv[i][j]);j++;}
             i++;
+            std::cout << " ";
         }
         std::cout << "\n";
     }
