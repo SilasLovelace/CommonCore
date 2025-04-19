da54502b81eb825ec6c4aac63f7b1648839ea757
silas <silas.oppermann@gmail.com>
2025-04-19 13:39:59 +0200

    cpp 08 done

--- CPP08/ex02/MutantStack.hpp
+++ CPP08/ex02/MutantStack.hpp
@@ -1,7 +1,36 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
+#include <stack>
 
-// template <typename T>
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+public:
+    // this extracts the underlying container type from the stack (deque) and the iterator from that
+    // so now the std::stack<T>::container_type is deque<T> and the iterator is deque<T>::iterator
+    typedef typename std::stack<T>::container_type::iterator iterator;
+
+    MutantStack() : std::stack<T>() {}
+    MutantStack(const MutantStack &other) : std::stack<T>(other) {}
+    ~MutantStack() {}
+
+    MutantStack &operator=(const MutantStack &other)
+    {
+        if (this != &other)
+            std::stack<T>::operator=(other);
+        return *this;
+    }
+
+    iterator begin()
+    {
+        return this->c.begin();
+    }
+
+    iterator end()
+    {
+        return this->c.end();
+    }
+};
 
 
 #endif
